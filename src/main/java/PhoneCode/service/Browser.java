6864ef2d6439a0c326c07321a764706707772d20
StanislavPB <perminovsb@gmail.com>
2022-06-18 01:58:55 +0300

    .

--- src/main/java/PhoneCode/service/Browser.java
+++ src/main/java/PhoneCode/service/Browser.java
@@ -2,31 +2,24 @@ package PhoneCode.service;
 
 import org.springframework.stereotype.Service;
 
-import java.awt.Desktop;
+
 import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
+
 
 @Service
 public class Browser {
-    public static void browser(String urlApp) {
+    public static void browser(String urlApp) throws IOException {
 
-        if(Desktop.isDesktopSupported()){
-            Desktop desktop = Desktop.getDesktop();
-            try {
-                desktop.browse(new URI(urlApp));
-            } catch (IOException | URISyntaxException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-        }else{
-            Runtime runtime = Runtime.getRuntime();
-            try {
-                runtime.exec("xdg-open " + urlApp);
-            } catch (IOException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
+        String os = System.getProperty("os.name").toLowerCase();
+
+        if (os.indexOf("win") >= 0) {Runtime rt = Runtime.getRuntime();
+            rt.exec("rundll32 url.dll,FileProtocolHandler " + urlApp);}
+
+        if (os.indexOf("mac") >= 0) {
+
+            Runtime rt = Runtime.getRuntime();
+
+            rt.exec("open " + urlApp);
         }
     }
 }
