76f6e31d439e068223ccc850f464048180abe37c
StanislavPB <perminovsb@gmail.com>
2022-06-20 19:34:17 +0300

    .

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -3,16 +3,20 @@ Vue.component('message-form', {
 
     template:
         '<div>' +
-        '<input type="text" placeholder="Write something" v-model="text" />' +
-        '<input type="button" value="Save" @click="save" />' +
-        '</div>',
+        '<label htmlFor="phone">Enter a phone number:</label>' +'<br>'+'<br>'+
 
+        '<input type="tel" id="phone" name="phone" placeholder="+123456-1234567" pattern="(?(\\+)?\\d{1,6})[-\\s]+(\\d{7})" required>'+'<br>'+'<br>'+
+        '<small>Format: 123-123-1234567</small>'+'<br>'+'<br>'+
+        '<input type="submit" value="Save">'+'<br>'+'<br>'+
+        '<input type="button" value="Exit" @click="exit" />'+
+        '</div>',
 });
 
 
 
 var app = new Vue({
     el: '#app',
+    template: '<message-form />',
     data: {
         message: 'Hello user'
     }
