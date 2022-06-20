398393ff5a9de4f5bc55c3413b93d43b33a34818
StanislavPB <perminovsb@gmail.com>
2022-06-21 00:01:39 +0300

    .

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -3,41 +3,20 @@ Vue.component('message-form', {
 
     template:
         '<div>' +
-
-        '<div>'+
-        '<h4>Enter your telephone number: </h4>'+
-        '<span className="countryDiv">'+
-        '"+"'+
-        '<input id="countryNo" aria-label="Country code (up  to 3 digits)" cols="20" name="countryNo"'+
-        'pattern="[0-9]{1,3}" placeholder="Country code (up  to 3 digits)"'+
+        '<h2>Enter your telephone number (11 - 13 digits): </h2>'+
+        '<span className="enterCountryDiv">'+
+        '+'+
+        '<input id="enterCountryNo" aria-label="Enter phone code" cols="20" name="enterCountryNo"'+
+        'pattern="[0-9]{11,13}" placeholder="Enter phone code"'+
         'required'+
         'type="tel"'+
-        'style="width: 175px;">'+
+        'style="width: 175px; font-size: large; text-align: right">'+
         '<span className="validity"></span>'+
         '</span>'+
-        '<span className="areaDiv">'+
-        '<input id="areaNo" aria-label="Area code (3 digits)" name="areaNo" pattern="[0-9]{3}"'+
-        'placeholder="Area code (3 digits)" required'+
-        'type="tel"'+
-        'style="width: 175px;">'+
-        '<span className="validity"></span>'+
-        '</span>'+
-
-        '<span className="numberDiv">'+
-        '<input id="number" aria-label="Phone part of number  (7 digits)" name="number" pattern="[0-9]{7}"'+
-        'placeholder="Phone part of number (7 digits)" required'+
-        'type="tel"'+
-        'style="width: 175px;"'+
-        '>'+
-        '<span className="validity"></span>'+
-        '</span>'+
-
-        '</div>'+
-
-        '<input type="submit" value="Save">'+'<br>'+'<br>'+
+        '<br>'+'<br>'+
+        '<input type="submit" value="Check country">'+'<br>'+'<br>'+
         '<input type="button" value="Exit" @click="exit" />'+
-        '</div>',
-});
+        '</div>',});
 
 
 
