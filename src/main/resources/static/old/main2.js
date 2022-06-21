a0aaec6177f3be61430139429505317474c5c243
StanislavPB <perminovsb@gmail.com>
2022-06-21 23:54:15 +0300

    .

--- src/main/resources/static/old/main2.js
+++ src/main/resources/static/old/main2.js
@@ -1,6 +1,8 @@
+/*
 var codesApi = Vue.resource('/codes{/id}');
 
-Vue.component('country-form', {
+Vue.component('phone-form', {
+
     props: ['countries'],
     data: function (){
       return {
@@ -8,32 +10,34 @@ Vue.component('country-form', {
       }
     },
 
-
     template:
         '<div>' +
         '<h2>Enter your telephone number (11 - 13 digits): </h2>'+
         '<span className="enterCountryDiv">'+
         '+'+
-        '<input id="enterCountryNo" aria-label="Enter phone code"  name="enterCountryNo" pattern="[0-9]{11,13}" placeholder="Enter phone code" v-model="phoneNumber" type ="tel" style="width: 175px; font-size: large; text-align: right" />'+
+        '<input id="enterCountryNo" aria-label="Enter phone code" cols="20" name="enterCountryNo"'+
+        'pattern="[0-9]{11,13}" placeholder="Enter phone code" v-model="phoneNumber" '+
+        'type ="tel"'+
+        'style="width: 175px; font-size: large; text-align: right">'+
         '<span className="validity"></span>'+
         '</span>'+
         '<br>'+'<br>'+
-        '<input type="button" value="Detect country" @click="detect" />'+
+        '<input type="button" value="Check country" @click="check">'+
         '<br>'+'<br>'+
         '<input type="button" value="Exit" @click="exit" />'+'<br>'+'<br>'+'<br>'+'<br>'+
         '<country-form :country="country"  />' +
         '</div>',
 
     methods:{
-        detect: function () {
-            var phone = {phoneNumber: this.phoneNumber};
+        check: function () {
+            var phone = this.phoneNumber;
 
             codesApi.save({}, phone).then(result =>
                 result.json().then(data => {
                     this.countries.push(data);
-                    this.phoneNumber = ''
-                }))},
-
+                    this.text = ''
+                })
+            )
 
         exit:function () {
             window.close();
@@ -51,7 +55,7 @@ Vue.component('country-form', {
     },
     template:
         '<div style="position: relative; width: 300px;">' +
-        '<country-form  />' +
+        '<phone-form countries="countries" />' +
         '{{ country.text }}' +
         '</div>',
     created: function() {
@@ -67,10 +71,12 @@ Vue.component('country-form', {
 
 var app = new Vue({
     el: '#app',
-    template: '<country-form />',
+    template: '<country-form: countries="countries" />',
     data: {
-        countr: 'Hello user'
+        countries: []
     }
 });
+
+ */
 
 
