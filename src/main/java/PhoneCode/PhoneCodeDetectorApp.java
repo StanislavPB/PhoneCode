0afdf2066ca7abe18aeb282fbc998caf5b06eaec
StanislavPB <perminovsb@gmail.com>
2022-06-29 13:12:41 +0300

    .

--- src/main/java/PhoneCode/PhoneCodeDetectorApp.java
+++ src/main/java/PhoneCode/PhoneCodeDetectorApp.java
@@ -7,8 +7,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PhoneCodeDetectorApp {
 
-    public static void main(String[] args) {
-        SpringApplication.run(PhoneCodeDetectorApp.class);
-    }
 
+
+    public static void main(String[] args) {
+        try {
+            SpringApplication.run(PhoneCodeDetectorApp.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
+
